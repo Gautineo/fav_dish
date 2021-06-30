b13ff8fa44541fc233bb80bdad38994876579f83
Gaurav Sati <gs250365@ncr.com>
2021-06-30 15:01:42 +0530

    First Commit

---
+++ app/src/main/java/com/gaurav/favdish/Splash.kt
@@ -0,0 +1,39 @@
+package com.gaurav.favdish
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
+import com.gaurav.favdish.databinding.ActivitySplashBinding
+
+
+class Splash : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val splashAnimation = AnimationUtils.loadAnimation(this, R.anim.anim_splash)
+        splashAnimation.fillAfter = true
+        binding.tvTest.animation = splashAnimation
+
+        splashAnimation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {}
+
+            override fun onAnimationEnd(animation: Animation?) {
+                Handler(Looper.getMainLooper()).postDelayed({
+                    startActivity(Intent(this@Splash, MainActivity::class.java))
+                    finish()
+                }, 1000)
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {}
+        })
+    }
+}
